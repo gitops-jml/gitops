c910a7b82a269e5d5cfd5ee9093fc5a93469200d
root <root@api.jml.cp.fyre.ibm.com>
2021-10-26 07:15:06 -0700

    add ENV to simplenodejs

--- app-src/simple-nodejs/app.js
+++ app-src/simple-nodejs/app.js
@@ -6,7 +6,7 @@ console.log("server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("Hello !\nYou've hit " + os.hostname() + "\n");
+  response.end("Hello !\nYou've hit " + os.hostname() + "   environment: " +  process.env.ENV +"\n");
 };
 
 var www = http.createServer(handler);
