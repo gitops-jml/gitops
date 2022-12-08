f49b016f5fe37a337d87ed3b8181788715c10aea
jmlafont <root@tower1.fyre.ibm.com>
2022-12-08 06:29:45 -0800

    add script to add access to private git repository

---
+++ add-argocd-repo.sh
@@ -0,0 +1,18 @@
+####################################################################################################################"
+# pre-req : 
+# - already logged to the OCP cluster
+# - port forwarding active : kubectl port-forward svc/openshift-gitops-cntk-server -n openshift-gitops 8080:443
+#
+# ARG1: user
+# ARG2: access token for the gitlab repository
+####################################################################################################################"
+
+#login to argocd
+#==========================================================================================================================================
+argocd login localhost:8080 --username admin --password $(kubectl -n openshift-gitops get secret openshift-gitops-cntk-cluster -o jsonpath="{['data']['admin\.password']}" | base64 -d ) --insecure
+
+#add the repository
+#==========================================================================================================================================
+argocd repo add https://gitlab.com/clarinsgroup/hg/eventing/phoenix-build.git  --username $1 --password $2
+
+
