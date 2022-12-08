eb9da83a275717f53595ec75cda942e47707e78e
jml <pubjml@free.fr>
2022-12-08 17:51:28 +0100

    Update add-argocd-repo.sh

--- add-argocd-repo.sh
+++ add-argocd-repo.sh
@@ -4,14 +4,14 @@
 # - port forwarding active : kubectl port-forward svc/openshift-gitops-cntk-server -n openshift-gitops 8080:443
 # - argocd CLI available: https://argo-cd.readthedocs.io/en/stable/cli_installation/
 #
-# ARG1: user
+# ARG1: gitlab user
 # ARG2: access token for the gitlab repository
 # ARG3: repository URL ( https://gitlab.com/clarinsgroup/hg/eventing/phoenix-build.git )
 ####################################################################################################################"
 
 #login to argocd
 #==========================================================================================================================================
-argocd login localhost:8080 --username admin --password $(kubectl -n openshift-gitops get secret openshift-gitops-cntk-cluster -o jsonpath="{['data']['admin\.password']}" | base64 -d ) --insecure
+argocd login localhost:8080 --username admin --password $(kubectl -n openshift-gitops get secret openshift-gitops -o jsonpath="{['data']['admin\.password']}" | base64 -d ) --insecure
 
 #add the repository
 #==========================================================================================================================================
